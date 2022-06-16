376cf34eeac04a6f700b58b2299f4ae235f844f3
Gerald Ncube <4821668+gncube@users.noreply.github.com>
2022-06-16 02:44:00 +0100

    Add project files.

---
+++ StringCalculator202206.UnitTests/StringCalculatorShould.cs
@@ -0,0 +1,11 @@
+namespace StringCalculator202206.UnitTests
+{
+    public class StringCalculatorShould
+    {
+        [Fact]
+        public void ReturnAnInteger_GivenAStringOfNumbers()
+        {
+            var 
+        }
+    }
+}
